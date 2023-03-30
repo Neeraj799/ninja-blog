13ad9c92fb61b93b568834fc5e159c0f48515393
neeraj <neerajpc799@gmail.com>
2023-03-30 17:22:18 +0530

    first commit

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,21 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'hello world...', author:'mario', id: 1 },
-        { title: 'Welcome party', body: 'hello world...', author:'Kohli', id: 2 },
-        { title: 'Web dev top tips', body: 'hello world...', author:'mario', id: 3 }
-    ]);
+    const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
-    const handleDelete = (id) => {
-      const newBlogs = blogs.filter(blog => blog.id !==id) ;
-      setBlogs(newBlogs); 
-    }
 
+  
+
+   
+  
     return ( 
         <div className="home">
-           <BlogList blogs={blogs} title="All Blogs"  handleDelete={handleDelete}/>
-           
+          { error && <div> { error } </div> }
+          { isPending && <div>Loading...</div> }
+          {blogs && <BlogList blogs={blogs} title="All Blogs"/>}
         </div>
      );
 }
