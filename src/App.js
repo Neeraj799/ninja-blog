196eef532a3ebca2dfaa416274304bcfcb96fd90
neeraj <neerajpc799@gmail.com>
2023-06-06 19:48:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import Navbar from './Navbar';
 import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <Navbar></Navbar>
       <div className="content">
-        <Home></Home>
+        <Switch>
+          <Route path="/">
+            <Home /> 
+          </Route>
+        </Switch>
       </div> 
     </div>
+    </Router>
   );
 }
 
--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,11 @@
 import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null);
-    const [isPending, setIsPending] = useState(true);
-    const [error, setError] = useState(null);
+  const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs');
 
 
-  
-
-   
-  
     return ( 
         <div className="home">
           { error && <div> { error } </div> }
